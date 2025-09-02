8944dc663c613d622a4770f03ed7c475d2d32ed2
bru1f0rc3 <wint1841@inbox.ru>
2025-09-02 22:01:00 +0300

    ready

---
+++ src/Main.java
@@ -0,0 +1,143 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Выберите: \n" +
+                    "1 - Первое задание\n" +
+                    "2 - Второе задание\n" +
+                    "3 - Третье задание\n\n" +
+                    "0 - Выход из программы");
+            Scanner in = new Scanner(System.in);
+            int select_task = in.nextInt();
+
+            switch (select_task) {
+                case 1:
+                    firstTask();
+                    break;
+                case 2:
+                    secondTask();
+                    break;
+                case 3:
+                    thirdTask();
+                    break;
+                case 0:
+                    return;
+                default:
+                    System.out.println("Возможно вы ввели не ту цифру...");
+            }
+        }
+    }
+
+    public static void firstTask(){
+        long inputGold = 200L;
+        byte inputSilver = 39;
+        short inputWood = 2005;
+        long inputHealth = 97L;
+        boolean inputHasHelper = true;
+
+        Resources characterResources = new Resources(
+                inputGold,
+                inputSilver,
+                inputWood,
+                inputHealth,
+                inputHasHelper
+        );
+
+        int characterGold = (int)characterResources.gold;
+        int characterSilver = characterResources.silver * 100;
+        double characterWood = characterResources.wood;
+        byte characterHealth = (byte)characterResources.health;
+        byte characterHelpersNumber = Byte.parseByte(characterResources.hasHelper? "1" : "0");
+
+        Character character = new Character(
+                characterGold,
+                characterSilver,
+                characterWood,
+                characterHealth,
+                characterHelpersNumber
+        );
+
+        System.out.println("Персонаж создан успешно!");
+        System.out.println("Количество золота: " + character.gold);
+        System.out.println("Количество серебра: " + character.silver);
+        System.out.println("Количество дерева: " + character.wood);
+        System.out.println("Здоровье: " + character.health);
+        System.out.println("Количество помощников: " + character.helpersNumber);
+        System.out.println("Навстречу приключениям!");
+    }
+
+    public static void secondTask(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя пользователя:");
+        String name = scanner.nextLine();
+        PersonAccount user = new PersonAccount(name);
+
+        System.out.println("Хотите открыть счёт в RUB?");
+        System.out.println("1 - Да");
+        System.out.println("2 - Нет");
+        int command = scanner.nextInt();
+        if (command == 1) {
+            user.moneyRUB = 0.0;
+        }
+
+        System.out.println("Хотите открыть счёт в USD?");
+        System.out.println("1 - Да");
+        System.out.println("2 - Нет");
+        command = scanner.nextInt();
+        if (command == 1) {
+            user.moneyUSD = 0.0;
+        }
+
+        System.out.println("Хотите открыть счёт в EUR?");
+        System.out.println("1 - Да");
+        System.out.println("2 - Нет");
+        command = scanner.nextInt();
+        if (command == 1)
+            user.moneyEUR = 0.0;
+
+
+        System.out.println("Поздравляем, аккаунт для пользователя " + user.name + " создан.");
+        System.out.println("Открытые счета:");
+        if (user.moneyRUB != null) {
+            System.out.println("- RUB");
+        }
+        if (user.moneyUSD != null) {
+            System.out.println("- USD");
+        }
+        if (user.moneyEUR != null) {
+            System.out.println("- EUR");
+        }
+    }
+
+    public static void thirdTask(){
+        String pixelKcalDay = "43";
+        String beefKcal = "30.2";
+        String chickenKcal = "23.8";
+        String creamKcal = "32.1";
+        String milkKcal = "13.5";
+
+        float pixelChoice = getMinKcalsSum(beefKcal, chickenKcal, creamKcal, milkKcal);
+        float totalKcal = Integer.parseInt(pixelKcalDay) + pixelChoice;
+        checkKcal(totalKcal);
+    }
+
+    private static Float getMinKcalsSum(String firstDishKcal, String secondDishKcal, String firstDesert, String secondDesert) {
+        float minDishKcal = Float.min(Float.parseFloat(firstDishKcal), Float.parseFloat(secondDishKcal));
+        float minDesertKcal = Float.min(Float.parseFloat(firstDesert),Float.parseFloat(secondDesert));
+        return minDishKcal + minDesertKcal;
+    }
+
+    private static void checkKcal(Float catKcal) {
+        if (catKcal == null) {
+            System.out.println("Что-то пошло не так");
+        } else {
+            System.out.println("Калорийность рациона Пикселя за день: " + catKcal);
+            if (catKcal > 100) {
+                System.out.println("Пиксель сегодня не уложился в норму.");
+            } else {
+                System.out.println("Лимит не превышен!");
+            }
+        }
+    }
+}
